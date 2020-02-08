fd91d7b9c29a051387cc1418a9da133b11842cfd
mrgrassho <mr.grassho@gmail.com>
2020-02-08 02:08:09 -0300

    Corrige backend.
    + Pendiente -> chequear varibles de entorno para mongobee en AppConfig.

--- geodiff/src/main/java/com/geodiff/AppConfig.java
+++ geodiff/src/main/java/com/geodiff/AppConfig.java
@@ -2,16 +2,19 @@ package com.geodiff;
 
 import com.github.mongobee.Mongobee;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Bean
+    public MyBean myBean () {
+        return new MyBean();
+    }
 
     @Bean
     public LayoutDialect layoutDialect() {
@@ -21,26 +24,42 @@ public class AppConfig {
     @Bean
     public Mongobee mongobee(){
         Mongobee runner = null;
-        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
-
-            Properties prop = new Properties();
-
-            // load a properties file
-            prop.load(input);
-
-            String connString = "mongodb://" +
-                            prop.getProperty("spring.data.mongodb.host") + ":" +
-                            prop.getProperty("spring.data.mongodb.port") + "/" +
-                            prop.getProperty("spring.data.mongodb.database");
-
-            runner = new Mongobee(connString);
-            runner.setChangeLogsScanPackage(prop.getProperty("spring.data.mongobee.changelogs")); // the package to be scanned for changesets
-
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        myBean().getMongoChangelogs();
+        String connString = "mongodb://" + myBean().getMongoHost() + ":" + myBean().mongoPort + "/"+ myBean().mongoDb;
+        runner = new Mongobee(connString);
+        runner.setChangeLogsScanPackage(myBean().getMongoChangelogs());
         return runner;
+    }
+
+    public static class MyBean {
+        @Value("${spring.data.mongodb.host}")
+        private String mongoHost;
+
+        @Value("${spring.data.mongodb.port}")
+        private String mongoPort;
+
+        @Value("${spring.data.mongodb.database}")
+        private String mongoDb;
+
+        @Value("${spring.data.mongobee.changelogs}")
+        private String mongoChangelogs;
+
+
+        public String getMongoHost() {
+            return mongoHost;
+        }
+
+        public String getMongoPort() {
+            return mongoPort;
+        }
+
+        public String getMongoDb() {
+            return mongoDb;
+        }
+
+        public String getMongoChangelogs() {
+            return mongoChangelogs;
+        }
     }
 
 
