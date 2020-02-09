992d9e8e69a46ab7b30f75c5431e31a2d798d1cc
mrgrassho <mr.grassho@gmail.com>
2020-02-09 09:24:59 -0300

    Agrega config. para container de MongoDB
    + Agrega creación de BD y roles para administración de base para que funcione con mongobee. Habia un problema de roles que no dejaba que se ejecuten las migrations.

---
+++ docker-deploy/mongo/entrypoint.js
@@ -0,0 +1,25 @@
+db.auth('root', 'example');
+
+db = db.getSiblingDB('geo-mongo');
+
+db.createRole({
+  role : "readWriteSystem",
+  privileges: [{
+    resource: {
+       db: "geo-mongo", collection: "system.indexes"
+    },
+    actions: [ "changeStream", "collStats", "convertToCapped", "createCollection", "createIndex", "dbHash", "dbStats", "dropCollection", "dropIndex", "emptycapped", "find", "insert", "killCursors", "listCollections", "listIndexes", "planCacheRead", "remove", "renameCollectionSameDB", "update" ]
+  }],
+  roles:[]}
+);
+
+db.createUser({
+  user: 'admin',
+  pwd: 'admin',
+  roles: [
+    {
+      role: 'readWriteSystem',
+      db: 'geo-mongo',
+    },
+  ],
+});
